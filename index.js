f9edbacb0b435d956a0d066887adb0342641d1b6
John Stilwell <johnrstilwell@codeimmersives.com>
2020-03-12 15:26:49 -0400

    added y

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 let x = 5
+
+let y = 6
+
+// term-2/test-git $ git branch second //!names the branch "second"
+// term-2/test-git $ git branch //! git branch shows ALL the branches
+// * master
+//   second
+// term-2/test-git $ git checkout second //! checkout second will switch to the second branch
+// Switched to branch 'second'
+// term-2/test-git $ git branch
+//   master
+// * second
+// term-2/test-git $ git checkout master
+// Switched to branch 'master'
+// term-2/test-git $ git branch
+// * master
+//   second
+// term-2/test-git $ git checkout -b third //! using this command will create the branch (third) and switch to it
+// Switched to a new branch 'third'
+// term-2/test-git $ git branch
+//   master
+//   second
+// * third
+// term-2/test-git $ 
